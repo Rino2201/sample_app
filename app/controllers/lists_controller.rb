630885fe3d89ac29d3344facb9a9293e9d1e9607
NoriakiNakamura <babababargxx@gmail.com>
2022-01-11 09:06:23 +0000

    first commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+      # 以下を追加
+    list = List.new(list_params)
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list.save
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    redirect_to'/top'
+    # 4. トップ画面へリダイレクト
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
