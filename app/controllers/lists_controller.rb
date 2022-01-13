e1a65b5b17ed854714f79533504330ee791dfffb
NoriakiNakamura <babababargxx@gmail.com>
2022-01-13 14:43:46 +0000

    rails 10章までの変更分

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,27 +5,42 @@ class ListsController < ApplicationController
   end
 
   def create
-      # 以下を追加
-    list = List.new(list_params)
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list.save
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    redirect_to'/top'
-    # 4. トップ画面へリダイレクト
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render:new
+    end
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   private
   #ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title,:body)
+    params.require(:list).permit(:title,:body,:image)
   end
 end
